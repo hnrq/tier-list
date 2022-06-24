81b0eb27ca785e164591f192f954b9243e290d67
Henrique Ramos <henriquealberone@outlook.com>
2022-06-24 02:50:57 -0300

    feat: create Sortable context

---
+++ src/context/sortable.tsx
@@ -0,0 +1,35 @@
+import {
+  Accessor,
+  Component,
+  createContext,
+  createMemo,
+  mergeProps,
+  ParentProps,
+  useContext,
+} from 'solid-js';
+
+import { Draggable, Sortable } from '@shopify/draggable';
+
+const SortableContext = createContext<Accessor<Draggable>>();
+
+interface SortableProviderProps {
+  options: typeof Sortable.arguments[1];
+  containers?: HTMLElement[];
+}
+
+export const SortableProvider: Component<ParentProps<SortableProviderProps>> = (
+  _props
+) => {
+  const props = mergeProps({ containers: [] }, _props);
+  const sortable = createMemo(
+    () => new Sortable(props.containers, props.options)
+  );
+
+  return (
+    <SortableContext.Provider value={sortable}>
+      {props.children}
+    </SortableContext.Provider>
+  );
+};
+
+export const useSortable = () => useContext(SortableContext);
