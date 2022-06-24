a8ce7cfee567348df29369629b64caf643672836
Henrique Ramos <henriquealberone@outlook.com>
2022-06-24 01:02:29 -0300

    feat: add vitest and husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,34 @@
-import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
-import tsConfigPaths from "vite-tsconfig-paths";
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig, UserConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [solidPlugin(), tsConfigPaths()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    transformMode: {
+      web: [/\.[jt]sx?$/],
+    },
+    setupFiles: './setupTests.ts',
+    // solid needs to be inline to work around
+    // a resolution issue in vitest:
+    deps: {
+      inline: [/solid-js/, /solid-testing-library/],
+    },
+    // if you have few tests, try commenting one
+    // or both out to improve performance:
+    threads: false,
+    isolate: false,
+  },
   build: {
-    target: "esnext",
+    target: 'esnext',
     polyfillDynamicImport: false,
   },
-});
+  resolve: {
+    conditions: ['development', 'browser'],
+  },
+} as UserConfig);
