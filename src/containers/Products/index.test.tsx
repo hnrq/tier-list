986b65eceac39b620360ec31b0ab6bf495f05fde
Henrique Ramos <henriquealberone@outlook.com>
2022-07-05 00:23:05 -0300

    feat: add collapsible menu to Products container

---
+++ src/containers/Products/index.test.tsx
@@ -0,0 +1,60 @@
+import { render, fireEvent } from 'solid-testing-library';
+
+import mockProducts from '__mocks__/products';
+import { SortableProvider } from 'context/sortable';
+
+import Products from '.';
+
+vi.mock('context/tierList', () => ({
+  useTierList: vi.fn(() => [{ unrankedProducts: mockProducts }]),
+}));
+
+const renderProducts = () =>
+  render(() => (
+    <SortableProvider options={{}}>
+      <Products />
+    </SortableProvider>
+  ));
+
+describe('<Products />', () => {
+  it('renders a title', () => {
+    const { getByText } = renderProducts();
+    expect(getByText('Products')).toBeInTheDocument();
+  });
+
+  it('renders a button to add new products', () => {
+    const { getByText } = renderProducts();
+    expect(getByText('Add Products')).toBeInTheDocument();
+  });
+
+  it('renders products from reducer', () => {
+    const { getByText } = renderProducts();
+
+    mockProducts.forEach((product) => {
+      expect(getByText(product.name)).toBeInTheDocument();
+    });
+  });
+
+  it('renders a button for toggling collapse in Products Menu', () => {
+    const { getByText } = renderProducts();
+    expect(getByText('Show Products')).toBeInTheDocument();
+  });
+
+  it("toggles button text between 'Show Products' and 'Hide Products'", async () => {
+    const { getByText, findByText } = renderProducts();
+    fireEvent.click(getByText('Show Products') as HTMLElement);
+
+    expect(await findByText('Hide Products')).toBeInTheDocument();
+  });
+
+  it('toggles container state when clicking outside of it', async () => {
+    const { getByText, findByText, container } = renderProducts();
+    fireEvent.click(getByText('Show Products') as HTMLElement);
+
+    expect(await findByText('Hide Products')).toBeInTheDocument();
+
+    fireEvent.click(container);
+
+    expect(await findByText('Show Products')).toBeInTheDocument();
+  });
+});
--- src/containers/Products/index.tsx
+++ src/containers/Products/index.tsx
@@ -1,12 +1,24 @@
-import { Component, For, onMount } from 'solid-js';
+import { Component, For, onMount, createSignal } from 'solid-js';
 
 import Product from 'components/Product';
 import { useSortable } from 'context/sortable';
 import { useTierList } from 'context/tierList';
+import clickOutsideDirective from 'directives/clickOutside';
 
 import './index.scss';
 
+declare module 'solid-js' {
+  namespace JSX {
+    interface Directives {
+      clickOutside: () => void;
+    }
+  }
+}
+
+const clickOutside = clickOutsideDirective;
+
 const Products: Component = () => {
+  const [showProducts, setShowProducts] = createSignal(false);
   const sortable = useSortable();
   const [tierList] = useTierList();
   let productsRef;
@@ -16,10 +28,26 @@ const Products: Component = () => {
   });
 
   return (
-    <div class="products" data-id="unranked" ref={productsRef}>
-      <For each={Object.values(tierList.unrankedProducts)}>
-        {(product) => <Product draggable {...product} />}
-      </For>
+    <div
+      class="products"
+      use:clickOutside={() => setShowProducts(false)}
+      classList={{ open: showProducts() }}
+    >
+      <button
+        class="button products__toggle"
+        onClick={() => setShowProducts((show) => !show)}
+      >
+        {showProducts() ? 'Hide' : 'Show'} Products
+      </button>
+      <div class="products__header">
+        <h2 class="products__title">Products</h2>
+        <button class="button button--link">Add Products</button>
+      </div>
+      <div class="products__container" data-id="unranked" ref={productsRef}>
+        <For each={tierList.unrankedProducts}>
+          {(product) => <Product draggable {...product} />}
+        </For>
+      </div>
     </div>
   );
 };
