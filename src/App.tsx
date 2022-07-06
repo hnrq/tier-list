fd860631b2bc04d2a327bcaae02e9ed1da20ec3b
Henrique Ramos <henriquealberone@outlook.com>
2022-07-06 16:18:23 -0300

    chore: add Directive namespace declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { Component } from 'solid-js';
+import { Component, VoidProps } from 'solid-js';
 
 import { SortableProvider } from 'context/sortable';
 import { TierListProvider } from 'context/tierList';
 import Home from 'pages/Home';
 
 import 'theme/index.scss';
+
+declare module 'solid-js' {
+  namespace JSX {
+    interface Directives {
+      clickOutside: () => void;
+      formSubmit: (form) => void;
+    }
+  }
+}
 
 const App: Component = () => (
   <SortableProvider options={{ draggable: '.product--draggable' }}>
