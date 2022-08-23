baa1f5cc6914cb3bf15d02dbbbee00bd9a703320
Henrique Ramos <henriquealberone@outlook.com>
2022-08-23 00:48:18 -0300

    feat: create stringChunk and hexToRGB util functions

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -1,7 +1,65 @@
+import { faker } from '@faker-js/faker';
+
 import * as utils from '.';
 
-describe('utils/randomColor()', () => {
+describe('utils#randomColor()', () => {
   it('generates a random color', () => {
     expect(/^#[0-9A-F]{6}$/i.test(utils.randomColor())).toBe(true);
+  });
+});
+
+describe('utils#stringChunk()', () => {
+  it('handles an empty string', () => {
+    expect(utils.stringChunk('', 2)).toEqual([]);
+  });
+
+  it('splits a string in equally sized substrings', () => {
+    const length = Math.floor(faker.datatype.number({ min: 10, max: 100 }) * 2);
+    const str = faker.random.alpha(length);
+
+    const chunkSize = 2;
+    const chunks = utils.stringChunk(str, chunkSize);
+
+    expect(chunks).toHaveLength(length / chunkSize);
+    chunks.forEach((chunk) => {
+      expect(chunk).toHaveLength(chunkSize);
+    });
+  });
+
+  it('leaves the remaining items in the last chunk', () => {
+    const str = faker.random.alpha(22);
+    const chunkSize = 3;
+
+    expect(utils.stringChunk(str, chunkSize).at(-1)).toHaveLength(
+      str.length % chunkSize
+    );
+  });
+});
+
+describe('utils#hexToRGB', () => {
+  it('throws an error when providing an invalid hex', () => {
+    expect(() => {
+      utils.hexToRGB('#fdsa');
+    }).toThrowError('Invalid hex');
+  });
+
+  it('parses a hex color to an RGB array', () => {
+    const color = '#ad57bd';
+    expect(utils.hexToRGB(color)).toEqual([173, 87, 189]);
+  });
+
+  it('parses a shorthand hex color to an RGB array', () => {
+    const color = '#fab';
+    expect(utils.hexToRGB(color)).toEqual([255, 170, 187]);
+  });
+});
+
+describe('utils#getContrastYIQ', () => {
+  it('returns white when YIQ brightness is smaller than 128', () => {
+    expect(utils.getContrastYIQ('#FFFFFF')).toBe('black');
+  });
+
+  it('returns black when YIQ brightness is bigger than or equal to 128', () => {
+    expect(utils.getContrastYIQ('#000')).toBe('white');
   });
 });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,41 @@
+export const stringChunk = (str: string, size: number): string[] => {
+  const numChunks: number = Math.ceil(str.length / size);
+  const chunks: string[] = new Array(numChunks);
+
+  for (let i = 0, offset = 0; i < numChunks; ++i, offset += size) {
+    chunks[i] = str.substring(offset, offset + size);
+  }
+
+  return chunks;
+};
+
 export const randomColor = () =>
   `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, '0')}`;
+
+export const hexToRGB = (hexColor: string) => {
+  const hex = hexColor.charAt(0) === '#' ? hexColor.substring(1) : hexColor;
+  if (hex.length !== 3 && hex.length !== 6) throw new Error('Invalid hex');
+  const fullHex = hex.length === 6;
+
+  return stringChunk(hex, fullHex ? 2 : 1).map((value) =>
+    parseInt(fullHex ? value : `${value}${value}`, 16)
+  );
+};
+
+export const getContrastYIQ = (hexColor: string): 'black' | 'white' => {
+  const rgb = hexToRGB(hexColor);
+  const yiq = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
+
+  return yiq >= 128 ? 'black' : 'white';
+};
+
+export const isValidUrl = (url: string) => {
+  try {
+    new URL(url);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
